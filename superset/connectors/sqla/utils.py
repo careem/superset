1b960ad09d3a8a3b06db0984f9542449ff2f9bd4
Rahul Raj <rahul.raj@careem.com>
2022-05-13 14:34:44 +0500

    fixed breaking changes

--- superset/connectors/sqla/utils.py
+++ superset/connectors/sqla/utils.py
@@ -51,13 +51,8 @@ from superset.utils.memoized import memoized
 if TYPE_CHECKING:
     from superset.connectors.sqla.models import SqlaTable
 
-COLUMN_EXPRESSION_MAPPINGS = {
-    "day": "cast(day as VARCHAR(10))"
-}
-
 PRESTO_DATABASE_NAME = "presto"
 
-MULTI_TABLE_COLUMN_DAY_LENGTH = 5
 
 
 def get_physical_table_metadata(
