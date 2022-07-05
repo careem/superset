db38cb8f0df15995cdeff36ea0af20865165bb3a
samraHanif0340 <samra.hanif@yahoo.com>
2022-07-05 15:47:22 +0500

    formatter for python added

--- tests/integration_tests/datasets/api_tests.py
+++ tests/integration_tests/datasets/api_tests.py
@@ -275,7 +275,9 @@ class TestDatasetApi(SupersetTestCase):
             "description": "Energy consumption",
             "extra": None,
             "fetch_values_predicate": None,
-            "filter_select_enabled": self.app.config["DEFAULT_FEATURE_FLAGS"]["UX_BETA"],
+            "filter_select_enabled": self.app.config["DEFAULT_FEATURE_FLAGS"][
+                "UX_BETA"
+            ],
             "is_sqllab_view": False,
             "kind": "physical",
             "main_dttm_col": None,
