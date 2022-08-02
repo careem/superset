923ee114a5503a8b891cf201f7ae3e004c530f86
Rahul Raj <rahul.raj@careem.com>
2022-08-02 22:14:46 +0500

    fix: reverted async mode

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -30,12 +30,16 @@ HYPHEN_SYMBOL='-'
 STATSD_PREFIX='careem.insights'
 
 gunicorn \
-    -k gevent \
     --bind "${SUPERSET_BIND_ADDRESS:-0.0.0.0}:${SUPERSET_PORT:-8088}" \
     --access-logfile "${ACCESS_LOG_FILE:-$HYPHEN_SYMBOL}" \
     --error-logfile "${ERROR_LOG_FILE:-$HYPHEN_SYMBOL}" \
     --workers ${SERVER_WORKER_AMOUNT:-2} \
+    --worker-class ${SERVER_WORKER_CLASS:-gthread} \
+    --threads ${SERVER_THREADS_AMOUNT:-20} \
     --timeout ${GUNICORN_TIMEOUT:-120} \
+    --keep-alive ${GUNICORN_KEEPALIVE:-2} \
+    --max-requests ${WORKER_MAX_REQUESTS:-0} \
+    --max-requests-jitter ${WORKER_MAX_REQUESTS_JITTER:-0} \
     --limit-request-line ${SERVER_LIMIT_REQUEST_LINE:-0} \
     --limit-request-field_size ${SERVER_LIMIT_REQUEST_FIELD_SIZE:-0} \
     --statsd-host=localhost:${STATSD_PORT} \
