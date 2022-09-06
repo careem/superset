9cff5e3e0a455d95cc099cb0a276d8c9cebc5b74
Rahul Raj <rahul.raj@careem.com>
2022-09-07 00:09:09 +0500

    fix: remove multiple flash in header

--- superset-frontend/src/views/App.tsx
+++ superset-frontend/src/views/App.tsx
@@ -44,13 +44,13 @@ let menu = {
   ...bootstrapData.common.menu_data,
 };
 
-let menuItem = {
-  name: 'Flash',
-  label: 'Flash',
-  url: '/flash/list/',
-};
+// let menuItem = {
+//   name: 'Flash',
+//   label: 'Flash',
+//   url: '/flash/list/',
+// };
 
-menu['menu'].push(menuItem);
+// menu['menu'].push(menuItem);
 
 let lastLocationPathname: string;
 
